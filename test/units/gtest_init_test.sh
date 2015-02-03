28f08beb7a253fa3984774fb7ad8c5a481c29cd4
cameron embree <csembree@gmail.com>
2015-02-03 23:55:43 +0000

    Added X11 support and ENV VAR path to Yaaraa base directory

--- test/units/gtest_init_test.sh
+++ test/units/gtest_init_test.sh
@@ -1,5 +1,7 @@
 export GTEST_HOME=../gtest
+export YAARAA_HOME=/home/pi/yaaraa
 export LD_LIBRARY_PATH=$GTEST_HOME/lib:$LD_LIBRARY_PATH
-g++ -I $GTEST_HOME/include -L $GTEST_HOME/lib -lgtest -lgtest_main -lpthread gtest_init_test.cpp
+g++ -std=c++0x -I $GTEST_HOME/include -L $GTEST_HOME/lib -lgtest -lgtest_main -lpthread gtest_init_test.cpp
+#g++ -I $YARAA_HOME/src $GTEST_HOME/include -L $GTEST_HOME/lib -lgtest -lgtest_main -lpthread gtest_init_test.cpp
 ./a.out
 rm a.out
